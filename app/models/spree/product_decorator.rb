6cf5f40e15728e5522e6a217ccd64a51caf3b4d5
Aubrey <aubrey.goodman@gmail.com>
2012-05-04 11:35:26 -0400

    missed another namespace

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,4 +1,4 @@
 # Add "Document" to Product Model
 Spree::Product.class_eval do
-  has_many :docs, :as => :viewable, :order => :position, :dependent => :destroy, :class_name => '::Doc'
+  has_many :docs, :as => :viewable, :order => :position, :dependent => :destroy, :class_name => 'Spree::Doc'
 end
