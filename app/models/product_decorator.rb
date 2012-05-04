944285f1238fdf9031dae9959ed2911c8218be6a
Aubrey Goodman <aubrey.goodman@gmail.com>
2012-05-03 23:57:02 -0300

    explicitly assigned class name to product.docs association

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -1,4 +1,4 @@
 # Add "Document" to Product Model
 Spree::Product.class_eval do
-  has_many :docs, :as => :viewable, :order => :position, :dependent => :destroy
+  has_many :docs, :as => :viewable, :order => :position, :dependent => :destroy, :class_name => 'Doc'
 end
