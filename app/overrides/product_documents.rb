cc86540db32365c0012e326f77bb3e2375d6286c
Aubrey <aubrey.goodman@gmail.com>
2012-05-04 12:03:32 -0400

    fixed a namespace error in partials

--- app/overrides/product_documents.rb
+++ app/overrides/product_documents.rb
@@ -1,10 +1,10 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_product_tabs",
                      :name => "converted_admin_product_tabs_41685004",
                      :insert_after => "[data-hook='admin_product_tabs'], #admin_product_tabs[data-hook]",
-                     :partial => "admin/products/docs",
+                     :partial => "spree/admin/products/docs",
                      :disabled => false)
 Deface::Override.new(:virtual_path => "products/show",
                      :name => "converted_product_properties_962473961",
                      :insert_after => "[data-hook='product_properties'], #product_properties[data-hook]",
-                     :partial => "products/product_documents",
+                     :partial => "spree/products/product_documents",
                      :disabled => false)
